d9994b123ac26ab581becd73eb47d76c722e60ba
jacamargo <johnycamargo15@gmail.com>
2024-04-25 19:20:33 -0500

    Se agrega el modelo de Materias_estudiantes

---
+++ WebApplication008/Models/Materias_estudiantesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication008.Models
+{
+    public class Materias_estudiantesModel
+    {
+        [Key]
+        public int Id_materia_estudiante { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Id de Materia")]
+        [DataType(DataType.Text)]
+        [Display(Name = "id_materia")]
+        [StringLength(10)]
+        public string Id_materia { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Id de Usuario")]
+        [DataType(DataType.Text)]
+        [Display(Name = "id_usuario")]
+        [StringLength(10)]
+        public string Id_Usuario { get; set; }
+    }
+}
